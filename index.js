20ab2b9131b7e400c8f6d62de5a5f7442515be93
Savindu@Pasintha <64083148+savindu-pasintha@users.noreply.github.com>
2021-07-10 21:07:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const server = http.createServer(app);
+
+const io = require('socket.io')(server,{
+  cors: {
+		origin: '*',
+		methods: ["GET", "POST"],
+		allowedHeaders: ["my-custom-header"],
+		credentials: true
+	 }
+}
+);
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/a', (req, res) => {
+app.get('/aa', (req, res) => {
   res.send("savindu pasintha");
 });
+
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
@@ -17,6 +30,6 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(port, () => {
+server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
